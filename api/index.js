80c9f620be6b0dcd4235f37310ad49c570e257ee
voxuanmaihoang@gmail.com <voxuanmaihoang@gmail.com>
2021-12-31 17:17:21 +0700

    Recommend

--- api/index.js
+++ api/index.js
@@ -11,10 +11,12 @@ import cors from 'cors';
 // import formatData from './training/formatData.js';
 // import createVectorsFromDocs from './training/createVectorsFromDocs.js';
 // import getCosineSimilarity from './training/getCosineSimilarity.js';
-
+// import getSimilarDocuments from './training/getSimilarDocuments.js';
 // const require = createRequire(import.meta.url);
 // const fs = require('fs');
 // const data = require('../data/data.json');
+// const result = require('./training/records/result.json');
+// const similarityResult = require('./training/records/similarity-result.json');
 
 const app = express();
 
@@ -25,8 +27,12 @@ mongoose
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-    .then(() => console.log('DB Connect Successfully!'))
+    .then(console.log('DB Connect Successfully!'))
     .catch((err) => console.log(err));
+
+// var uniqueSimilarityResult = [...new Set(similarityResult)];
+// var connection = mongoose.connection;
+// connection.collection('recommends').insertMany(uniqueSimilarityResult, {ordered : false });
 
 app.use(express.json());
 app.use(cors());
@@ -35,12 +41,26 @@ app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/movies', movieRoute);
 app.use('/api/lists', listRoute);
-app.use('/api/recommend', recommendRoute);
+app.use('/api/recommends', recommendRoute);
 
 app.listen(8000, () => {
     console.log('Backend server is running');
+
     // const formattedData = formatData(data);
     // const dataVectors = createVectorsFromDocs(formattedData);
     // const result = getCosineSimilarity(dataVectors);
+    // console.log(result);
     // fs.writeFileSync('result.json', JSON.stringify(result));
+
+    // const values = Object.values(result);
+    // let array = [];
+    // for (let i = 0; i < values.length; i++) {
+    //     const value = values[i];
+    //     for (let j = 0; j < value.length; j++) {
+    //         array.push(value[j]);
+    //     }
+    // };
+
+    // fs.writeFileSync('similarity-result.json', JSON.stringify(array));
+    // connection.collection('recommends').insertMany(uniqueSimilarityResult, {ordered : false });
 });
--- api/models/Recommend.js
+++ api/models/Recommend.js
@@ -1,13 +1,19 @@
 import mongoose from 'mongoose';
 
 const RecommendSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        unique: true
+    },
     title: {
         type: String,
-        required: true,
-        unique: true
+        required: true
     },
     image: {
         type: String
+    },
+    score: {
+        type: Number
     }
 },
     { timestamps: true }
