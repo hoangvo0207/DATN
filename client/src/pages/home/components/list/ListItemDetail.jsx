9cfae86bc7d7100107c9f60de0f2f5ad1b0237e2
voxuanmaihoang@gmail.com <voxuanmaihoang@gmail.com>
2022-01-11 17:49:03 +0700

    Refactor UI Movie Detail

--- client/src/pages/home/components/list/ListItemDetail.jsx
+++ client/src/pages/home/components/list/ListItemDetail.jsx
@@ -1,45 +1,67 @@
 import { makeStyles } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import LocalOfferIcon from '@material-ui/icons/LocalOffer';
-import StartIcon from '@material-ui/icons/Star';
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import Navbar from '../navbar/Navbar';
 
 const useStyles = makeStyles(() => ({
+    emptyFirst: {
+        backgroundColor: '#00202e',
+        height: 125
+    },
     root: {
-        height: '100vh',
-        marginTop: 70,
-        backgroundColor: '#0c222f',
+        backgroundColor: '#00202e'
     },
-    contentLeft: {
+    paper: {
+        width: '75%',
         margin: 'auto',
-        marginTop: 50
     },
-    contentRight: {
-        color: 'white',
-        marginTop: 70,
-        textAlign: 'left',
-    },
-    contentMain: {
-        marginTop: 20
+    grid: {
+        height: 590
     },
     img: {
-        marginLeft: '35%',
-        width: 500,
-        height: 700,
-        borderRadius: '5%'
+        height: 590,
+        width: '85%',
+        borderRadius: 5
     },
-    button: {
+    content: {
+        padding: 20
+    },
+    play: {
         width: 200,
-        height: 50,
-        marginTop: 20
+        display: 'flex',
+        fontSize: 18,
+        margin: '16px 10px 16px 0px',
+        backgroundColor: '#0a344a',
+        color: 'white'
     },
-    intro: {
-        marginTop: 10
+    link: {
+        textDecoration: 'none'
+    },
+    text: {
+        width: 520,
+        fontSize: 14,
+        marginTop: 16
+    },
+    tags: {
+        marginTop: 10,
+        marginBottom: 10
+    },
+    title: {
+        fontStyle: 'italic'
+    },
+    emptyLast: {
+        backgroundColor: '#00202e',
+        height: 70
+    },
+    icon: {
+        marginLeft: 50
     }
 }));
 
@@ -51,47 +73,55 @@ const ListItemDetail = () => {
     return (
         <React.Fragment>
             <Navbar />
-            <Grid container justifyContent='center' className={classes.root} spacing={2}>
-                <Grid item xs={12} md={6} className={classes.contentLeft}>
-                    <img src={movie.img} alt='movie-img' className={classes.img} />
-                </Grid>
-                <Grid item xs={12} md={6} className={classes.contentRight}>
-                    <Typography variant='h2'>
-                        {movie.title}
-                    </Typography>
-                    <div className={classes.intro}>
-                        <StartIcon />
-                        <StartIcon />
-                        <StartIcon />
-                        <StartIcon />
-                        <StartIcon />
-                        <LocalOfferIcon style={{ marginLeft: 50 }} /> {movie.genre}
-                        <AccessTimeIcon style={{ marginLeft: 50 }} /> {movie.createdAt}
-                    </div>
-                    <Link className='link' to={{ pathname: '/watch', movie: movie }}>
-                        <Button variant='contained' color='secondary' className={classes.button}>
-                            Play
-                        </Button>
-                    </Link>
-                    <div className={classes.contentMain}>
-                        <Typography variant='subtitle1'>
-                            Genre: {movie.genre}
-                        </Typography>
-                        <Typography variant='subtitle1'>
-                            Type: {movie.isSeries ? 'Series' : 'Movie'}
-                        </Typography>
-                        <Typography variant='subtitle1'>
-                            Limit: {movie.limit}+
-                        </Typography>
-                        <Typography variant='subtitle1'>
-                            Year: {movie.year}
-                        </Typography>
-                        <Typography variant='subtitle1'>
-                            Description: {movie.desc}
-                        </Typography>
-                    </div>
-                </Grid>
-            </Grid>
+            <div className={classes.emptyFirst}></div>
+            <div className={classes.root}>
+                <Paper className={classes.paper}>
+                    <Grid container justifyContent='center' className={classes.grid}>
+                        <Grid item xs={12} md={6}>
+                            <img src={movie.img} alt='movie-img' className={classes.img} />
+                        </Grid>
+                        <Grid item xs={12} md={6} className={classes.content}>
+                            <Typography className={classes.title} variant='h3' color='textSecondary'>
+                                {movie.title}
+                            </Typography>
+                            <Link className={classes.link} to={{ pathname: '/watch', movie: movie }}>
+                                <Button className={classes.play} variant='contained'>
+                                    Play
+                                </Button>
+                            </Link>
+                            <div>
+                                <Typography variant='subtitle1' color='textSecondary'>
+                                    Genre: {movie.genre}
+                                </Typography>
+                                <Typography variant='subtitle1' color='textSecondary'>
+                                    Type: {movie.isSeries ? 'Series' : 'Movie'}
+                                </Typography>
+                                <Typography variant='subtitle1' color='textSecondary'>
+                                    Limit: {movie.limit}+
+                                </Typography>
+                                <Typography variant='subtitle1' color='textSecondary'>
+                                    Year: {movie.year}
+                                </Typography>
+                                <div className={classes.tags}>
+                                    <LocalOfferIcon /> {movie.genre}
+                                    <AccessTimeIcon className={classes.icon} /> {movie.createdAt}
+                                </div>
+                                <TextField
+                                    id='outlined-basic'
+                                    label='Description'
+                                    variant='outlined'
+                                    rows={10}
+                                    multiline
+                                    disabled
+                                    className={classes.text}
+                                    defaultValue={movie.desc}
+                                />
+                            </div>
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </div>
+            <div className={classes.emptyLast}></div>
         </React.Fragment>
     );
 };
